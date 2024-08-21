943578c52e939021e4c3ee75453827983cdb3dcb
Ry <162963838+RyXeleron@users.noreply.github.com>
2024-08-21 19:50:33 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
 -- security reasons
 -- plus infinite store is discontinued and no longer maintained don't use it
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/Ryxeleron/infiniteyield-reborn/master/source"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fuckusfm/infiniteyield-reborn/master/source"))()
