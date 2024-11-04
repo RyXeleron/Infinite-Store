72267dd401f42c91d69562afabb89abefbc0b7fd
Real_Ry <162963838+RyXeleron@users.noreply.github.com>
2024-11-04 17:50:40 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- Security reasons
 -- yo Toon you're the goat
 -- loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/Ryxeleron/infiniteyield-reborn/master/source"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fuckusfm/infiniteyield-reborn/master/source"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ryxeleron/infiniteyield-reborn/master/source"))()
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/fuckusfm/infiniteyield-reborn/master/source"))()
